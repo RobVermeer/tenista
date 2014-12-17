310e4053377bc85403034bb8c33d33692a67515c
Rob Vermeer <rob@vermeertech.nl>
2014-12-17 03:42:17 +0100

    Register header and footer menu

--- inc/menus.php
+++ inc/menus.php
@@ -2,11 +2,10 @@
 
 /* This theme uses wp_nav_menu() */
 add_theme_support('nav_menus');
-/*
+
 register_nav_menus( array(
 	'header' => 'Header navigatie',
 	'footer' => 'Footer navigatie'
 ) );
-*/
 
 ?>
