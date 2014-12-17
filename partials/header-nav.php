92dd2faf446cd281f293a978bc36903c5fb02946
Rob Vermeer <rob@vermeertech.nl>
2014-12-17 03:42:25 +0100

    Add header nav

---
+++ partials/header-nav.php
@@ -0,0 +1,14 @@
+<header class="row header">
+	<div class="col-xs-12 header-img">
+		<a href="<?php bloginfo('url'); ?>"><img src="http://placehold.it/1200x300"></a>
+		<div class="cta">
+			<a href="#" class="btn btn-default"><?php _e('Word nu lid!', 'rm'); ?></a>
+			<nav>
+				<?php _e('Amsterdam', 'rm'); ?> | <a href="mailto:info@tenista.nl"><?php _e('info@tenista.nl', 'rm'); ?></a>
+			</nav>
+		</div>
+	</div>
+	<nav class="col-xs-12">
+		<?php wp_nav_menu(array('theme_location' => 'header', 'menu_id' => '', 'container' => '', 'container_class' => '', 'fallback_cb' => '', 'depth' => 1)); ?>
+	</nav>
+</header>
