3a1b4c836045bb6a16ae6090d6cc125e3dc4b2f6
Rob Vermeer <rob@vermeertech.nl>
2014-12-17 04:40:50 +0100

    Fix page content loop

--- partials/page-content.php
+++ partials/page-content.php
@@ -1,4 +1,6 @@
-<article class="entry col-xs-12 <?php echo (has_children() ? 'col-sm-9' : ''); ?>">
-	<h1><?php the_title(); ?></h1>
-	<?php the_content(); ?>
-</article>
+<?php if( have_posts() ) while( have_posts() ) : the_post(); ?>
+	<article class="entry col-xs-12 <?php echo (has_children() ? 'col-sm-9' : ''); ?>">
+		<h1><?php the_title(); ?></h1>
+		<?php the_content(); ?>
+	</article>
+<?php endwhile; ?>
