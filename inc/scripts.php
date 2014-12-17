4ba33c9476f3e61d17d33a8f1b6f528f35b3a75a
Rob Vermeer <rob@vermeertech.nl>
2014-12-17 02:35:30 +0100

    Add script and styles functions

---
+++ inc/scripts.php
@@ -0,0 +1,33 @@
+<?php
+
+/* Register all scripts */
+function register_scripts() {
+	$deps = array();
+
+	/* No l10n.js */
+	wp_deregister_script( 'l10n' );
+	
+	/* No default jQuery */
+	wp_deregister_script('jquery');
+	
+	/* jQuery via Google CDN */
+	wp_enqueue_script('jquery', "//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js", false, null, true);
+	$deps[] = 'jquery';
+
+	/* Add Bootstrap javascript *
+	$url = get_bloginfo('stylesheet_directory') . '/js/bootstrap.min.js';
+	$file = get_template_directory() . '/js/bootstrap.min.js';
+	wp_enqueue_script('bootstrap-js', $url, $deps, filemtime($file), true);
+	$deps[] = 'bootstrap-js';
+
+	/* Add functions javascript *
+	$deps = array('jquery');
+	$url = get_bloginfo('stylesheet_directory') . '/js/functions.js';
+	$file = get_template_directory() . '/js/functions.js';
+	wp_enqueue_script('functions-js', $url, $deps, filemtime($file), true);
+	
+	/**/
+}
+add_action('wp_enqueue_scripts', 'register_scripts');
+
+?>
---
+++ inc/styles.php
@@ -0,0 +1,21 @@
+<?php
+
+function register_styles() {
+	$deps = array();
+	
+	/* Add bootstrap stylesheet *
+	$url = get_bloginfo('stylesheet_directory') . '/css/bootstrap.min.css';
+	$file = get_template_directory() . '/css/bootstrap.min.css';
+	wp_enqueue_style('bootstrap', $url, $deps, filemtime($file));
+	
+	/* Add theme stylesheet *
+	$url = get_bloginfo('stylesheet_directory') . '/css/style.css';
+	$file = get_template_directory() . '/css/style.css';
+	wp_enqueue_style('style', $url, $deps, filemtime($file));
+	
+	/**/
+}
+
+add_action('wp_enqueue_scripts', 'register_styles');
+
+?>
