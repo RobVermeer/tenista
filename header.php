4e8edb5b00c35b9be0052eb9a801c59674c0a545
Rob Vermeer <rob@vermeertech.nl>
2014-12-17 02:45:50 +0100

    Add header

---
+++ header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+
+<html <?php language_attributes(); ?>>
+
+<head>
+
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/img/favicon.ico">
+	
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<?php wp_head(); ?>
+	
+</head>
+
+<body <?php body_class("no-js"); ?>>
+	<?php do_action('after_body'); ?>
+	
+	<?php get_template_part('partials/header', 'nav'); ?>
