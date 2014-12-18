56cdaa4322e84e6036750a1951430ebb88b6a6f0
Rob Vermeer <rob@vermeertech.nl>
2014-12-18 05:40:40 +0100

    Add home content

---
+++ partials/home-content.php
@@ -0,0 +1,9 @@
+<div class="col-xs-12 col-sm-9">
+	<?php get_template_part('partials/home', 'slider'); ?>
+	
+	<article class="entry">
+		<?php if( have_posts() ) while( have_posts() ) : the_post(); ?>
+			<?php the_content(); ?>
+		<?php endwhile; ?>
+	</article>
+</div>
