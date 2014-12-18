b64013f4c73491e80a5a3410cccdf830d3df803b
Rob Vermeer <rob@vermeertech.nl>
2014-12-18 05:40:15 +0100

    Add front-page

---
+++ front-page.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+	<section class="row content">
+		<?php get_template_part('partials/home', 'content'); ?>
+		
+		<?php get_template_part('partials/home', 'news'); ?>
+	</section>
+
+<?php get_footer(); ?>
