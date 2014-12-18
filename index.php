0b8a74497fca3ae67efd62a02d62d6b7f3649c18
Rob Vermeer <rob@vermeertech.nl>
2014-12-18 04:55:43 +0100

    Add archive index

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php get_header(); ?>
 
-
+	<section class="row content">
+		<?php get_template_part('partials/news', 'archive'); ?>
+		
+		<?php get_template_part('partials/news', 'sidebar'); ?>
+	</section>
 
 <?php get_footer(); ?>
