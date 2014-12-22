47802c9d7b38852da5f2aad03df4bcb8f94312c6
Rob Vermeer <rob@vermeertech.nl>
2014-12-22 01:37:38 +0100

    Change footer

--- partials/footer-nav.php
+++ partials/footer-nav.php
@@ -1,11 +1,17 @@
 <footer class="row footer">
-	<nav class="col-xs-12 col-sm-6">
-		<a href="http://www.sportcentrum.vu.nl/"><?php _e('Sportcentrum VU', 'rm'); ?></a>
-		<a href="http://www.studentensportamsterdam.nl/"><?php _e('Studentensport Amsterdam', 'rm'); ?></a>
-	</nav>
-	<div class="col-xs-12 col-sm-6 info">
-		&copy; <?php bloginfo('name'); ?> <?php echo date_i18n('Y', current_time('timestamp')); ?><br>
-		<?php _e('Studententennis<br>Amsterdam<br>', 'rm'); ?>
-		<a href="mailto:info@tenista.nl"><?php _e('info@tenista.nl', 'rm'); ?></a>
+	<div class="col-xs-12">
+		<div class="inner">
+			<div class="row">
+				<nav class="col-xs-12 col-sm-8 logos">
+					<a href="http://www.sportcentrum.vu.nl/"><img src="<?php bloginfo('template_url'); ?>/img/vu.png" alt="<?php _e('Sportcentrum VU', 'rm'); ?>"></a>
+					<a href="http://www.studentensportamsterdam.nl/"><img src="<?php bloginfo('template_url'); ?>/img/ssa.png" alt="<?php _e('Studentensport Amsterdam', 'rm'); ?>"></a>
+				</nav>
+				<div class="col-xs-12 col-sm-4 info">
+					<span>&copy; <?php bloginfo('name'); ?> <?php echo date_i18n('Y', current_time('timestamp')); ?></span><br>
+					<?php _e('Studententennis<br>Amsterdam<br>', 'rm'); ?>
+					<a href="mailto:info@tenista.nl"><?php _e('info@tenista.nl', 'rm'); ?></a>
+				</div>
+			</div>
+		</div>
 	</div>
 </footer>
