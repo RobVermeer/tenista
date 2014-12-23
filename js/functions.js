d093c3da401dbc6d976ddfecd02087109ed50f7c
JasperLagemaat <jasperlagemaat@gmail.com>
2014-12-23 15:14:29 +0100

    Add fixed bg to large screens
    #56

--- js/functions.js
+++ js/functions.js
@@ -7,6 +7,8 @@
 	var viewportWidth = $(window).width();
 	
 	if (viewportWidth > 992) {
+		$body.add(" <div class='fixed-bg'><img src='../img/IMG_2223.jpg' /></div> ");
+		
 		$(window).scroll(function(){ 
 			if ($(this).scrollTop() > 313) { 
 				$('.page-nav ul').addClass('fixed'); 
