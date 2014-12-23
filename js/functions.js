6409dd484200c9d719851ea73f30b11995c30a46
JasperLagemaat <jasperlagemaat@gmail.com>
2014-12-23 17:31:13 +0100

    Update scrolling page-nav minder script
    #52

--- js/functions.js
+++ js/functions.js
@@ -4,17 +4,12 @@
 
 	$body.removeClass('no-js');
 	
-	var viewportWidth = $(window).width();
-	
-	if (viewportWidth > 992) {
-	
-		$(window).scroll(function(){ 
-			if ($(this).scrollTop() > 313) { 
-				$('.page-nav ul').addClass('fixed'); 
-			} else { 
-				$('.page-nav ul').removeClass('fixed'); 
-			}
-		});
-	}
+	$(window).scroll(function(){ 
+		if ($(this).scrollTop() > 313) { 
+			$('.page-nav ul').addClass('fixed'); 
+		} else { 
+			$('.page-nav ul').removeClass('fixed'); 
+		}
+	});
 	
 })(window.jQuery);
