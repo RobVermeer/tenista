e28ab9e5855b152c727a9f53d27b715a96fc8a62
JasperLagemaat <jasperlagemaat@gmail.com>
2014-12-27 10:00:29 +0100

    Create form-content.php
    #71

---
+++ partials/form-content.php
@@ -0,0 +1,148 @@
+<div class="row">
+	<div class="col-sm-3 col-sm-push-9">
+		Het lidmaatschap duurt van september tot september en de contributie bedraagt:
+		<table class="table">
+			<thead>
+				<tr>
+					<td style="width: 400px;">Gehele jaar:</td>
+					<td>€170</td>
+				</tr>
+				<tr>
+					<td>Vanaf april:</td>
+					<td>€135</td>
+				</tr>
+			</thead>
+		</table>
+		De contributie maak je over naar:
+		<strong>NL80 INGB 0005 4483 31 t.a.v. SVU Tenista
+		O.v.v. naam en contributie</strong>
+		Voor vragen kan je mailen naar info@tenista.nl.
+	</div>
+	<div class="col-sm-9 col-sm-pull-3">
+		<form class="form-horizontal">
+			<div class="form-group">
+				<label class="col-sm-3 control-label" for="geslacht">Geslacht</label>
+				<div class="col-sm-9">
+					<label class="radio-inline">
+						<input id="geslacht" name="inlineRadioOptions" type="radio" value="Man" />Man
+					</label>
+					<label class="radio-inline">
+						<input id="geslacht" name="inlineRadioOptions" type="radio" value="Vrouw" />Vrouw
+					</label>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-3 control-label" for="voornaam">Voornaam</label>
+				<div class="col-sm-9">
+					<input id="voornaam" class="form-control" type="text" placeholder="Voornaam" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-3 control-label" for="achternaam">Achternaam</label>
+				<div class="col-sm-9">
+					<input id="achternaam" class="form-control" type="text" placeholder="Achternaam" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-3 control-label" for="geboortedatum">Geboortedatum</label>
+				<div class="col-sm-9">
+					<input id="geboortedatum" class="form-control" type="date" placeholder="Geboortedatum" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-3 control-label" for="adres">Adres</label>
+				<div class="col-sm-9">
+					<input id="adres" class="form-control" type="text" placeholder="Adres" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-3 control-label" for="telefoon">Telefoonnummer</label>
+				<div class="col-sm-9">
+					<input id="telefoon" class="form-control" type="number" placeholder="Telefoonnummer" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-3 control-label" for="postcode">Postcode en plaats</label>
+				<div class="col-sm-9">
+					<input id="postcode" class="form-control" type="text" placeholder="Postcode en plaats" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-3 control-label" for="email">Email</label>
+				<div class="col-sm-9">
+					<input id="email" class="form-control" type="email" placeholder="Email" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-3 control-label" for="student">Student</label>
+				<div class="col-sm-9">
+					<label class="radio-inline">
+						<input id="student" name="inlineRadioOptions" type="radio" value="Ja" />Ja
+					</label>
+					<label class="radio-inline">
+						<input id="student" name="inlineRadioOptions" type="radio" value="Nee" />Nee
+					</label>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-3 control-label" for="universiteit">Universiteit</label>
+				<div class="col-sm-9">
+					<input id="universiteit" class="form-control" type="text" placeholder="Universiteit" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-3 control-label" for="studie">Studie / Werk</label>
+				<div class="col-sm-9">
+					<input id="studie" class="form-control" type="text" placeholder="Studie / Werk" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-3 control-label" for="commissie">Interesse in een commissie?</label>
+				<div class="col-sm-9">
+					<select id="commissie" class="form-control">
+						<option>- - - - -</option>
+						<option>Evenementen commissie</option>
+						<option>Pinkstertoernooi commissie</option>
+						<option>Weekendjeweg commissie</option>
+						<option>Amsterdams Studenten TennisToernooi commissie</option>
+						<option>Media commissie</option>
+						<option>Nee bedankt, geen interesse</option>
+					</select>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-3 control-label" for="knltb">KNLTB nummer (indien al eerder KNLTB-lid)</label>
+				<div class="col-sm-9">
+					<input id="knltb" class="form-control" type="number" placeholder="KNLTB nummer" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-3 control-label" for="single">Speelsterkte (voor beginners 9)</label>
+				<div class="col-sm-9">
+					<div class="row">
+						<div class="col-xs-4">
+							<input type="number" id="single" class="form-control" placeholder="Enkel" />
+						</div>
+						<div class="col-xs-4">
+							<input type="number" id="dubbel" class="form-control" placeholder="Dubbel" />
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-3 control-label" for="foto">Recente foto</label>
+				<div class="col-sm-9">
+					<input id="foto" type="file" />
+					<p class="help-block">Indien je nog geen KNLTB-lid bent of een verouderde foto bij de KNLTB bekend is, dient een foto ingeleverd te worden!</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-3"></div>
+				<div class="col-sm-9">
+					<button class="btn btn-default" style="background:#00008c;color:white;border:none;padding:9px 16px;float:left;" type="submit">Verzenden</button>
+					<button class="btn btn-default" style="background:#ddd;color:black;border:none;padding:9px 16px;margin-left:10px;" type="reset">Reset</button>
+				</div>
+			</div>
+		</form>
+	</div>
+</div>
