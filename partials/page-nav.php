27d226df0024c501577c3e630f7b3239857d1f46
JasperLagemaat <jasperlagemaat@gmail.com>
2015-01-24 21:32:49 +0100

    Add page-nav button
    #83

--- partials/page-nav.php
+++ partials/page-nav.php
@@ -1,5 +1,10 @@
 <?php if( has_children() ) : ?>
 	<nav class="col-xs-12 col-sm-3 page-nav">
+		<button class="navbar-toggle collapsed btn btn-default visible-xs" type="button" data-toggle="collapse" data-target=".page-nav">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
 		<?php $children = get_the_children(); ?>
 		<ul>
 			<?php foreach( $children as $child ) : ?>
