9eb412e1dc9a6b28143e4ae96aab4ed3d9a3ca7a
Rob Vermeer <rob@vermeertech.nl>
2015-01-26 22:31:20 +0100

    Change image size

--- inc/images.php
+++ inc/images.php
@@ -3,7 +3,7 @@
 /* Post thumbnail support */
 add_theme_support('post-thumbnails');
 add_image_size('thumbnail-wide', 900, 500, true);
-add_image_size('thumbnail-small', 450, 250, true);
+add_image_size('thumbnail-small', 450, 350, true);
 
 /* Content width for max width images */
 //if ( ! isset( $content_width ) ) $content_width = 560;
