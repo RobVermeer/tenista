c93d433df0f06461d54eef1ca3e61fb6c610cd8d
Rob Vermeer <rob@vermeertech.nl>
2015-01-26 19:25:50 +0100

    Change exclude sticky in carousel
    Fixes #79

--- partials/home-slider.php
+++ partials/home-slider.php
@@ -2,6 +2,7 @@
 $featured = new WP_Query(array(
 	'post_type' => 'post',
 	'posts_per_page' => -1,
+	'post__not_in' => get_option('sticky_posts'),
 	'meta_query' => array(
 		array(
 			'key' => 'featured',
