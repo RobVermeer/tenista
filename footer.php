1947b4e37174d3ce5499e46e953d9f1387c7e673
JasperLagemaat <jasperlagemaat@gmail.com>
2016-09-19 21:37:01 +0200

    Upload banner code

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 		<?php get_template_part('partials/footer', 'nav'); ?>
 		<div class="page-border-bottom"></div>
+	<script language=JavaScript src=http://www1.sportadvertentie.nl/banners/abm.asp?z=1265></script>
 	</div>
-	
+
 	<?php wp_footer(); ?>
 	
 </body>
