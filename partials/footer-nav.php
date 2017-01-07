345efde1aaa1c7abdf1c0b58613341e8790ad859
JasperLagemaat <jasperlagemaat@gmail.com>
2017-01-07 12:36:21 +0100

    Replacing DeKroeg logo with Suprisebar

--- partials/footer-nav.php
+++ partials/footer-nav.php
@@ -5,7 +5,7 @@
 				<nav class="col-xs-12 col-sm-10 logos">
 					<a href="http://www.sportcentrum.vu.nl/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/vu.png" alt="<?php _e('Sportcentrum VU', 'rm'); ?>"></a>
 					<a href="http://www.studentensportamsterdam.nl/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/ssa.png" alt="<?php _e('Studentensport Amsterdam', 'rm'); ?>"></a>
-					<a href="https://www.facebook.com/pages/Cafe-de-Kroeg/267191183310332" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/dekroeg.png" alt="<?php _e('Cafe de Kroeg', 'rm'); ?>"></a>
+					<a href="https://www.facebook.com/surprisebar.amsterdam/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/surprisebar.png" alt="<?php _e('Surprisebar Amsterdam', 'rm'); ?>"></a>
 					<a href="http://itennis.nl/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/logo-itennis.png" alt="<?php _e('iTennis', 'rm'); ?>"></a>
 				</nav>
 				<div class="col-xs-12 col-sm-2 info">
